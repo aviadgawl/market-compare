9d55437f24587074eca2fc115717d6d48a517c4e
Aviad Shua <AviadS@call.local>
2020-06-28 12:27:51 +0300

    i have nothing to say

---
+++ src/components/search-form/products-api-service-credentials.js
@@ -0,0 +1,3 @@
+export default class ProductsApiServiceCredentials{
+    url = "https://marketcomapreapi.azurewebsites.net/api/GetProducts?code=kPOEGGsokLoaff40eD82uYA2eRgZRGNWYU2DH75zvMcyoVCAga4aFQ==";
+}
